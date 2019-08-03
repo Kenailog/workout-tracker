516f9a97a3e63d5f5af6d3955c91a2f31d3ea43a
Paweł Golianek <p.w.golianek@gmail.com>
2019-08-04 00:31:53 +0200

    add Activity entity class

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,16 +4,27 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Activity;
 
-class MainController extends AbstractController
+class ShowController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/show", name="show")
      */
     public function index()
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+        // $em = $this->getDoctrine()->getManager();
+
+        // $activity = new Activity();
+        // $activity->setType("run")->setDuration(new \DateTime("01:00:25"))->setKcal(825)->setDate(new \DateTime('12-17-2019'));
+        // $em->persist($activity);
+        // $em->flush();
+
+        return $this->render('show/index.html.twig', [
+            'type' => 'run',
+            'duration' => '01:00:25',
+            'kcal' => 825,
+            'date' => '12-17-2019',
         ]);
     }
 }
---
+++ src/Entity/Activity.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ActivityRepository")
+ */
+class Activity
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $duration;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $kcal;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDuration(): ?\DateTimeInterface
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(\DateTimeInterface $duration): self
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getKcal(): ?int
+    {
+        return $this->kcal;
+    }
+
+    public function setKcal(int $kcal): self
+    {
+        $this->kcal = $kcal;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
