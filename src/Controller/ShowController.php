521a87215e1fd4fac11b10d4fa90997051da5b84
Paweł Golianek <p.w.golianek@gmail.com>
2019-08-04 21:50:00 +0200

    add remove button to remove an object

--- src/Controller/ShowController.php
+++ src/Controller/ShowController.php
@@ -2,10 +2,7 @@
 
 namespace App\Controller;
 
-use App\Form\NewActivityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Activity;
 
@@ -32,28 +29,6 @@ class ShowController extends AbstractController
             'duration' => '01:00:25',
             'kcal' => 825,
             'date' => '12-17-2019',
-        ]);
-    }
-
-    /**
-     * @Route("/show/{id}/edit", name="edit")
-     * @param Request $request
-     * @param $id
-     * @return Response
-     */
-    public function edit(Request $request, $id) {
-        $activity = $this->getDoctrine()->getRepository(Activity::class)->find($id);
-        $form = $this->createForm(NewActivityType::class, $activity);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->flush();
-            return $this->redirectToRoute('show');
-        }
-
-        return $this->render('add/index.html.twig', [
-            'form' => $form->createView()
         ]);
     }
 }
